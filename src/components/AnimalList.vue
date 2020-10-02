b6e0b3291d4a5cd9b5630ca3fb08d417f278941f
Dusan Jankovic <dusanjankovic877@gmail.com>
2020-10-02 10:41:11 +0200

    created animal list and printins all animals in table

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -1,15 +1,57 @@
 <template>
   <div class="hello">
-
+    <table>
+      <tr>
+        <th>sort</th>
+        <th>name</th>
+        <th>date</th>
+      </tr>
+      <tr v-for="(animal, index) in AnimalList" :key="index">
+        <td>{{animal.sort}}</td>
+        <td>{{animal.name}}</td>
+        <td>{{animal.dateOfBirth.toLocaleString()}}</td>
+        
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AnimalList',
-  props: {
-    msg: String
+  data() {
+    return {
+      AnimalList: [
+        {
+          sort: 'Zebra',
+          name: 'Mico',
+          dateOfBirth: new Date(2020, 1, 6)
+        },
+
+                {
+          sort: 'Tigar',
+          name: 'Simba',
+          dateOfBirth: new Date(2020, 1, 6)
+        },
+        {
+          sort: 'Panda',
+          name: 'Kung Fu',
+          dateOfBirth: new Date(2020, 1, 6)
+        },
+        {
+          sort: 'Zirafa',
+          name: 'Milica',
+          dateOfBirth: new Date(2020, 1, 6)
+        },
+        {
+          sort: 'Slon',
+          name: 'Dambo',
+          dateOfBirth: new Date(2020, 1, 6)
+        }
+      ]
+    }
   }
+
 }
 </script>
 
@@ -28,5 +70,23 @@ li {
 }
 a {
   color: #42b983;
+}
+
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 30%;
+  margin-left: 35%;
+  margin-right: 35%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
 }
 </style>
